93b15bd834bda4eede29a1823a5c3a2a5673c121
AlexanderDracka <alexander.dracka@profiq.cz>
2017-02-20 20:00:16 +0100

    edited webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-require('./style.css');
-var print = require('./print.js');
-document.write(print);
-console.log('ahoj');
+//var print = require('./print.js');
+//document.write(print);
+console.log('ahoj sefino');
+console.log('chuj')
+
+
+//require('./style.css');
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import {install} from '@extjs/reactor';
+//import {Panel} from '@extjs/reactor/modern';
+
+ReactDOM.render(
+    <h1>Hello World!</h1>,
+    document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,34 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+//const ExtJSReactWebpackPlugin = require('@extjs/reactor-webpack-plugin');
+
 module.exports={
     devtool:'eval',
-    devServer:{hot:true},
-    entry:['./src/index.js', './src/style.css'],
+    devServer:{
+        contentBase:'./public',
+        hot:true},
+    entry:[
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.js',
+        './src/style.css'],
     output:{
         path: __dirname + '/dist',
         filename:'./bundle.js'
+    },
+    resolve:{
+        extensions:['*','.js','.jsx']
     },
     module:{
         loaders:[
             {
                 test:/\.css$/,
                 loader:ExtractTextPlugin.extract({fallback:'style-loader',use:'css-loader'})
-            },{
-                test:/\.jsx$/,
-                loader:'babel-loader'
-            }
+            },
+            { test: /\.js$/, loaders: ['react-hot-loader', 'babel-loader'], exclude: /node_modules/ },
+            { test: /\.jsx$/, loaders:['react-hot-loader','babel-loader'], exclude: /node_modules/ }
+
         ]
         /*loaders: [
             {
@@ -32,6 +43,12 @@ module.exports={
         new HtmlWebpackPlugin({
             template:'./src/index.html'
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        /*new ExtJSReactWebpackPlugin({
+            sdk:'c:\\Users\\ToolsTeam\\Documents\\Architect\\frameworks\\ext62\\6.2.1.167\\commercial',
+            theme:'theme-material',
+            packages:['ux']
+        })*/
+
     ]
 };
